c13765db16626a6973ff645d8ff0119d2dbcf82e
Midouni Billel <midounibillel@gmail.com>
2018-11-26 15:32:54 +0100

    init

---
+++ src/Joueur.java
@@ -0,0 +1,12 @@
+public class Joueur {
+
+    private int grainePossedees;
+
+    public Joueur() {
+        this.grainePossedees = 0;
+    }
+
+    public static void addGraines(int i) {
+
+    }
+}
---
+++ src/Position.java
@@ -0,0 +1,7 @@
+class Position {
+    int cells_player[] = new int[12]; // each cell contains a certain number of seeds
+    int cells_computer[] = new int[12];
+    boolean computer_play; // boolean true if the computer has to play and false otherwise
+    int seeds_player; // seeds taken by the player
+    int seeds_computer; // seeds taken by the computer
+}
