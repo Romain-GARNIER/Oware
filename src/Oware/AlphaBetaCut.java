114ba93d93e95ed4178d22449c25bf80e8b7b5fb
Midouni Billel <midounibillel@gmail.com>
2018-12-17 09:37:11 +0100

    AlphaBetaCut version 2

--- src/Oware/AlphaBetaCut.java
+++ src/Oware/AlphaBetaCut.java
@@ -1,77 +1,67 @@
 package Oware;
 
 public class AlphaBetaCut {
-    int AlphaBetaCutValue(Position pos_current, boolean computer_play, int depth, int depthMax, int alpha, int beta){
-        int[] tab_values = new int[6];
-        int bestCell = 1;
+    private int bestCell;
+    private int[] tab_values;
 
-        Position pos_next = new Position(); // In C : created on the stack: = very fast
-        if (GameControler.finalPosition(pos_current, computer_play, depth)){
-            // WRITE the code: returns VALMAX (=96) if the computer wins, -96 if it loses; 0 if draw
-            for (int i = 0; i < pos_current.cells_player.length; i++){
-                pos_current.seeds_player += pos_current.cells_player[i];
+    int[] coupPossible(Position pos_current, boolean computer_play){
+        int[] coupsPossible = new int[6];
+        for (int i=0; i<6; i++){
+            if(GameControler.validMove(pos_current, computer_play, i)){
+                coupsPossible[i] = 1;
+            } else {
+                if (computer_play) coupsPossible[i] =-100;
+                else coupsPossible[i]=+100;
             }
-
-            for (int i = 0; i < pos_current.cells_computer.length; i++){
-                pos_current.seeds_computer += pos_current.cells_computer[i];
-            }
-            if(pos_current.seeds_computer > pos_current.seeds_player)
-                return 96;
-            if(pos_current.seeds_computer < pos_current.seeds_player)
-                return -96;
-            if(pos_current.seeds_computer == pos_current.seeds_player)
-                return 0;
         }
-        if (depth == depthMax) {
+        return coupsPossible;
+    }
+
+    int AlphaBetaCutValue(Position pos_current, boolean computer_play, int depth, int depthMax, int a, int b){
+        this.tab_values = new int[6];
+        int[] coupsPossible = new int[6];
+
+        Position pos_next; // In C : created on the stack: = very fast
+
+        if (depth == depthMax || GameControler.finalPosition(pos_current, computer_play, depth)) {
             int evaluation = GameControler.evaluation(pos_current);
             IHM.log("evaluation : "+evaluation+"\n",3);
             return evaluation;
             // the simplest evealution fucntion is the difference of the taken seeds
         }
 
-        for(int i=0;i<6;i++){ // /!\ Question : on essaie de faire 6 coups (parcourt du camp) ou 12 coups (parcourt du plateau) ?
-            // we play the move i
-            // WRITE function validMove(pos_current, computer_play,i)
-            // it checks whether we can select the seeds in cell i and play (if there is no seed the function returns false)
-            if (GameControler.validMove(pos_current, computer_play,i)){
-                // WRITE function playMove(&pos_next,pos_current, computer_play,i)
-                // we play the move i from pos_current and obtain the new position pos_next
-                pos_next = GameControler.playMove(pos_current, computer_play,i);
-                // pos_next is the new current poisition and we change the player
-                tab_values[i]=AlphaBetaCutValue(pos_next,!computer_play,depth+1, depthMax, alpha, beta);
-            } else {
-                if (computer_play) tab_values[i]=-100;
-                else tab_values[i]=+100;
-            }
-        }
-        int res = tab_values[0];
+        int alpha = a;
+        int beta = b;
 
-        if (depth == 0){
-            int indice = 0;
-            // WRITE the code: res contains the MAX of tab_values
-            for(int i=0;i<tab_values.length;i++){
-                if(res < tab_values[i]){
-                    res = tab_values[i];
-                    indice = i;
-                }
+        if (computer_play){
+            for(int i =0; i<tab_values.length; i++){
+                pos_next = GameControler.playMove(pos_current, computer_play, i);
+                int value = AlphaBetaCutValue(pos_next, !computer_play, depth+1, depthMax, alpha, beta);
+                alpha = Math.max(alpha, value);
+                tab_values[i] = Math.max(tab_values[i], value);
+                if (alpha >= beta) return beta;
             }
-            IHM.log("AlphaBetaCut :" + tab_values[indice],1);
-            res = indice;
-            return res;
+            return alpha;
         }else{
-            for (int i=0; i < tab_values.length; i++){
-                if (computer_play){
-                    if (res < tab_values[i]){
-                        if (beta > tab_values[i]) beta = tab_values[i];
-                        res = tab_values[i];
-                    }
-                }else{
-                    if (res > tab_values[i]){
-                        if (alpha < tab_values[i]) alpha = tab_values[i];
-                        res = tab_values[i];
-                    }
-                }
-                if (alpha >= beta) break;
+            for(int i=0; i<tab_values.length;i++){
+                pos_next = GameControler.playMove(pos_current, computer_play, i);
+                int value = AlphaBetaCutValue(pos_next, !computer_play, depth+1, depthMax, alpha, beta);
+                beta = Math.min(beta, value);
+                tab_values[i] = Math.min(tab_values[i], value);
+                if(alpha>=beta) return alpha;
+            }
+            return beta;
+        }
+    }
+
+    int runAlphaBetaCut(Position pos_current, boolean computer_play, int depthMax){
+        AlphaBetaCutValue(pos_current, computer_play, 0, depthMax, -100, 100);
+        int res = this.tab_values[0];
+        int indice = 0;
+        for(int i=0;i<this.tab_values.length;i++){
+            if(res < this.tab_values[i]){
+                res = this.tab_values[i];
+                indice = i;
             }
         }
 
@@ -83,6 +73,11 @@ public class AlphaBetaCut {
         IHM.log("tab value : "+str_tab,2);
         IHM.log("res : "+res,2);
         IHM.log("-------\n",2);
-        return computer_play? beta : alpha;
+
+        IHM.log("MinMax :"+tab_values[indice],1);
+        res = indice;
+        return res;
     }
+
+
 }
